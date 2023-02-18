0527b66303276de3697579f700cdaff3df98983f
Tran Long <long.pham.tran2612@gmail.com>
2023-02-18 22:28:11 +0700

    ++ dto

---
+++ src/main/java/com/example/reactivepostgresql/model/dto/request/UpdateUserRequest.java
@@ -0,0 +1,22 @@
+package com.example.reactivepostgresql.model.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+/**
+ * 2:10 PM 18-Feb-23
+ * Long Tran
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UpdateUserRequest {
+
+    private String name;
+
+    private Integer age;
+
+}
