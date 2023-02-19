f02cc611c07df5f59a9e1612391238d5d42e6a92
Tran Long <long.pham.tran2612@gmail.com>
2023-02-19 12:40:52 +0700

    ++ error code

--- src/main/java/com/example/reactivepostgresql/exception/ErrorCode.java
+++ src/main/java/com/example/reactivepostgresql/exception/ErrorCode.java
@@ -5,4 +5,11 @@ package com.example.reactivepostgresql.exception;
  * Long Tran
  */
 public class ErrorCode {
+    private static final String USER_NOT_FOUND = "USER_NOT_FOUND";
+    private static final String GROUP_NOT_FOUND = "GROUP_NOT_FOUND";
+    private static final String USER_NAME_INVALID = "USER_NAME_INVALID";
+    private static final String USER_AGE_INVALID = "USER_AGE_INVALID";
+    private static final String GROUP_NAME_INVALID = "GROUP_NAME_INVALID";
+
+
 }
